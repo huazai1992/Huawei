e5012a60a21c52bc10ea5f1ef99cc79be1ca1141
朱志华 <zhuzhihua@zhuzhihuadeMacBook-Pro.local>
2018-10-25 10:54:31 +0800

    1025

--- CPU_test/Pstate_test.py
+++ CPU_test/Pstate_test.py
@@ -2,52 +2,49 @@
 
 import cProfile
 import pstats
+import random
+import argparse
 
-num = 100000000
-
-
-
-def IntOps():
+def IntOps(num):
     # 加法
-    factor_add = 0
     for i in range(num):
-        a = factor_add + i
+        a = random.randint(0, num) + random.randint(0, num)
 
     # 乘法
-    factor_mul= 1
     for i in range(num):
-        a = factor_mul * i
+        a = random.randint(0, num) * random.randint(0, num)
 
     # 除法
-    factor_div = num
-    for i in range(1, num+1):
-        a = factor_div / i
-
-
-def FloatOps():
-    # 加法
-    factor_add = 0.
     for i in range(num):
-        a = factor_add + i
+        a = random.randint(0, num) / random.randint(1, num)
+
+
+def FloatOps(num):
+    # 加法
+    for i in range(num):
+        a = random.uniform(0, num) + random.uniform(0, num)
 
     # 乘法
-    factor_mul = 1.
     for i in range(num):
-        a = factor_mul * i
+        a = random.uniform(0, num) * random.uniform(0, num)
 
     # 除法
-    factor_div = float(num)
     for i in range(1, num+1):
-        a = factor_div / i
+        a = random.uniform(0, num) / random.uniform(1, num)
 
 
-def main():
-    IntOps()
+def main(num):
+    IntOps(num)
 
-    FloatOps()
+    FloatOps(num)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="your script description")
+    parser.add_argument('--ops-number')
+
+    args = parser.parse_args()
+    num = args.ops_number
     cProfile.run(
-        'main()',"./CPU_test_result/Pstate_ops{}_powersave_disable.out".format(num))
-    p = pstats.Stats("./CPU_test_result/Pstate_ops{}_powersave_disable.out".format(num))
-    p.strip_dirs().sort_stats('cumtime').print_stats()
+        'main({})'.format(num),"./CPU_test_result/Pstate_ops{}_enable.out".format(num))
+    # p = pstats.Stats("./CPU_test_result/Pstate_ops{}_powersave_disable.out".format(num))
+    # p.strip_dirs().sort_stats('cumtime').print_stats()
